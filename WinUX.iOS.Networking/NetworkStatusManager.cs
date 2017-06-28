22e09069d5a8557252d24a4868685b9362dbffa4
James Croft <jamz_c@hotmail.co.uk>
2017-06-28 13:21:21 +0100

    Added classes to flesh out network status managers for Android and iOS

---
+++ WinUX.iOS.Networking/NetworkStatusManager.cs
@@ -0,0 +1,35 @@
+﻿namespace WinUX.Networking
+{
+    public class NetworkStatusManager : INetworkStatusManager
+    {
+        /// <inheritdoc />
+        public event NetworkStatusChangedEventHandler NetworkStatusChanged;
+
+        /// <inheritdoc />
+        public NetworkStatus CurrentNetworkStatus
+            =>
+                new NetworkStatus(
+                    this.CurrentNetworkProfileName,
+                    this.CurrentConnectionType,
+                    this.CurrentMobileNetworkConnectionType,
+                    this.CurrentNetworkSignal);
+
+        /// <inheritdoc />
+        public string CurrentNetworkProfileName { get; private set; }
+
+        /// <inheritdoc />
+        public NetworkConnectionType CurrentConnectionType { get; private set; }
+
+        /// <inheritdoc />
+        public MobileNetworkConnectionType CurrentMobileNetworkConnectionType { get; private set; }
+
+        /// <inheritdoc />
+        public byte? CurrentNetworkSignal { get; private set; }
+
+        /// <inheritdoc />
+        public void Initialize()
+        {
+            // ToDo; access iOS network status
+        }
+    }
+}
